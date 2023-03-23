f004631f93f91dd901f9a559457253b80e715b59
Macbook <sbhs6884@gmail.com>
2023-03-22 17:51:22 -0700

    remove default from extend

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,8 +63,6 @@ public class RobotContainer {
  
   private final Joystick m_joystick = new Joystick(0);
   private final CommandXboxController controller = new CommandXboxController(1);
-  private final Command m_extendCommand = new PivotCommand(m_pivotSubsystem,
-  () -> m_joystick.getY());
   private final Command m_pivotCommand = new PivotCommand(m_pivotSubsystem,
       () -> m_joystick.getY());
   private final ClawCmmd m_clawCommand = new ClawCmmd(
@@ -95,7 +93,6 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     m_pivotSubsystem.setDefaultCommand(m_pivotCommand);
-    m_extendSubsystem.setDefaultCommand(m_extendCommand);
     eventMap = new HashMap<>();
     eventMap.put("autobalance", autoBalance);
     m_gyro.calibrate();
